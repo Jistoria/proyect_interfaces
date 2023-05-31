92a7877c94b97eb26ea5155fc19f377149d5a114
iwachunaruchi <133074732+iwachunaruchi@users.noreply.github.com>
2023-05-31 15:06:42 -0600

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -42,3 +42,85 @@ function vaciarCarrito() {
   carrito = [];
   mostrarCarrito();
 }
+const usuarioString = localStorage.getItem('usuario');
+const usuario = JSON.parse(usuarioString);
+console.log(usuario); // Imprime el objeto usuario en la consola
+
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      prueba: 'Bienvenido!',
+        usuario: JSON.parse(localStorage.getItem('usuario')) || {},
+        errors: [],
+        nombre: '',
+        apellido:'',
+        email:'',
+        telefono:'',
+        direccion:'',
+        contraseña:'',
+        r_contraseña:''
+    }
+    
+
+  },
+  methods:{
+    checkForm: function (e) {
+      
+      
+
+      this.errors = [];
+      if (!this.nombre) {
+        this.errors.push('El nombre es obligatorio.');
+      }
+      if (!this.email) {
+        this.errors.push('El email es obligatorio.');
+      }
+      if (!this.apellido) {
+        this.errors.push('El apellido es obligatorio.');
+      }
+      if (!this.telefono) {
+        this.errors.push('El numero te telefono es obligatorio.');
+      }
+      if (!this.direccion) {
+        this.errors.push('La direccion es obligatoria.');
+      }
+      if (!this.contraseña) {
+        this.errors.push('La contraseña es obligatoria.');
+      }
+      if (!this.r_contraseña) {
+        this.errors.push('Escriba la contraseña de nuevo.');
+      }
+      if (this.contraseña !== this.r_contraseña) {
+        this.errors.push('Las contraseñas no coinciden');
+      }
+      if (this.errors.length === 0) {
+        // Crear objeto usuario con los datos del formulario
+          const usuario = {
+          nombre: this.nombre,
+          apellido: this.apellido,
+          email: this.email,
+          telefono: this.telefono,
+          direccion: this.direccion,
+          contraseña: this.contraseña
+        };
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+        window.location.href = 'login.html';
+      }
+e.preventDefault();
+}
+
+
+  }
+}).mount('#app')
+function realizarCompra() {
+// Aquí puedes agregar la lógica para procesar la compra, como enviar una solicitud al servidor, guardar la información en una base de datos, etc.
+
+// Por ahora, simplemente mostraremos un mensaje de confirmación
+alert("¡Gracias por tu compra!");
+
+// También puedes restablecer el carrito o redirigir al usuario a una página de confirmación de compra, dependiendo de tus necesidades.
+}
+
