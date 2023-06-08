fabc21d3790286b09a9c6a49a9bb69b572b56af7
CarloPadilla1 <jeanblabla_14@hotmail.com>
2023-06-07 19:47:18 -0500

    a

--- admin.js
+++ admin.js
@@ -1,22 +1,24 @@
 const formAgregarProducto = document.querySelector('.agregar-producto form');
 formAgregarProducto.addEventListener('submit', function(event) {
-event.preventDefault(); // Evitar que se recargue la página al enviar el formulario
-const nombre = document.getElementById('nombre').value;
-const precio = parseFloat(document.getElementById('precio').value);
-agregarProducto(nombre, precio);
-formAgregarProducto.reset(); // Limpiar el formulario
+  event.preventDefault();
+  const nombre = document.getElementById('nombre').value;
+  const precio = parseFloat(document.getElementById('precio').value);
+  const imagen = document.getElementById('imagen').value || "https://via.placeholder.com/200x200" ;
+  agregarProducto(nombre, precio, imagen);
+  formAgregarProducto.reset();
 });
-function agregarProducto(nombre, precio) {
-const productos = document.querySelector('.productos');
-const nuevoProducto = `
-  <div class="producto">
-    <img src="https://via.placeholder.com/200x200" alt="${nombre}">
-    <h2>${nombre}</h2>
-    <p>$${precio}</p>
-    <button class="eliminar" onclick="eliminarProducto(this)">Eliminar</button>
-  </div>
-`;
-productos.insertAdjacentHTML('beforeend', nuevoProducto);
+
+function agregarProducto(nombre, precio, imagen) {
+  const productos = document.querySelector('.productos');
+  const nuevoProducto = `
+    <div class="producto">
+      <img src="${imagen}" alt="${nombre} style="width: 200px; height: 200px;">
+      <h2>${nombre}</h2>
+      <p>$${precio}</p>
+      <button class="eliminar" onclick="eliminarProducto(this)">Eliminar</button>
+    </div>
+  `;
+  productos.insertAdjacentHTML('beforeend', nuevoProducto);
 }
 
 function eliminarProducto(boton) {
